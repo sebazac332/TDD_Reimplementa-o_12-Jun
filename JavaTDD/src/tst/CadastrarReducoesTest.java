557351d2856d860295773d212c854a4baf0107fe
Sebastian Zuzunaga <seb55381@hotmail.com>
2025-06-12 17:04:30 -0500

    primeiro teste adicionado no CadastrarReducoes

--- JavaTDD/src/tst/CadastrarReducoesTest.java
+++ JavaTDD/src/tst/CadastrarReducoesTest.java
@@ -2,13 +2,24 @@ package tst;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
+
+import app.IRPF;
 
 public class CadastrarReducoesTest {
 
+    private IRPF irpf; 
+
+    @Before
+    public void setup(){
+        irpf = new IRPF();
+    }
+
     @Test
-    public void test(){
-        fail("not implemented yet");
+    public void testCadastrarPrevidenciaOficial(){
+        irpf.CadastrarPrevidenciaOficial(700f);
+        assertEquals(700f, irpf.getPrevidenciaOficial(), 0);
     }
     
 }
