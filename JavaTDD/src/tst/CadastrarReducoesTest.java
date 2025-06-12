4755bc38193fafb4f6959e8fdac000c788cc1de4
Sebastian Zuzunaga <seb55381@hotmail.com>
2025-06-12 17:00:09 -0500

    criacao de arquivo de testes para reducoes

---
+++ JavaTDD/src/tst/CadastrarReducoesTest.java
@@ -0,0 +1,14 @@
+package tst;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CadastrarReducoesTest {
+
+    @Test
+    public void test(){
+        fail("not implemented yet");
+    }
+    
+}
