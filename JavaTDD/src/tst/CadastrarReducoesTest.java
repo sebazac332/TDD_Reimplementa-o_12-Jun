f19aba743c24a776c1b5e0337f8cdc0ea0db57aa
Sebastian Zuzunaga <seb55381@hotmail.com>
2025-06-12 18:53:41 -0500

    todos testes de reducoes rodando

--- JavaTDD/src/tst/CadastrarReducoesTest.java
+++ JavaTDD/src/tst/CadastrarReducoesTest.java
@@ -21,5 +21,17 @@ public class CadastrarReducoesTest {
         irpf.CadastrarPrevidenciaOficial(700f);
         assertEquals(700f, irpf.getPrevidenciaOficial(), 0);
     }
+
+    @Test
+    public void testCadastrarPrevidenciaOficial2(){
+        irpf.CadastrarPrevidenciaOficial(800f);
+        assertEquals(800f, irpf.getPrevidenciaOficial(), 0);
+    }
+
+    @Test
+    public void testCadastrarPrevidenciaOficial3(){
+        irpf.CadastrarPrevidenciaOficial(900f);
+        assertEquals(900f, irpf.getPrevidenciaOficial(), 0);
+    }
     
 }
