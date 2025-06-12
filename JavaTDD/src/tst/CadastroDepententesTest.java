3726789b435898f04b2e3b1f84c3979876d256ce
Sebastian Zuzunaga <seb55381@hotmail.com>
2025-06-12 18:56:53 -0500

    criacao de arquivo de testes de dependentes

---
+++ JavaTDD/src/tst/CadastroDepententesTest.java
@@ -0,0 +1,24 @@
+package tst;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import app.IRPF;
+
+public class CadastroDepententesTest {
+
+    private IRPF irpf; 
+
+    @Before
+    public void setup(){
+        irpf = new IRPF();
+    }
+
+    @Test
+    public void test(){
+        fail("not yet implemented");
+    }
+    
+}
