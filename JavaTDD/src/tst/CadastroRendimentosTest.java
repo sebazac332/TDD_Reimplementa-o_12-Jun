10ab471dc892a5df37603d90989600f9bf6055b2
Sebastian Zuzunaga <seb55381@hotmail.com>
2025-06-12 16:54:27 -0500

    adicioado teste CadastrarSalario3

--- JavaTDD/src/tst/CadastroRendimentosTest.java
+++ JavaTDD/src/tst/CadastroRendimentosTest.java
@@ -27,4 +27,11 @@ public class CadastroRendimentosTest {
         irpf.cadastrarSalario(6000f);
         assertEquals(6000f, irpf.getTotalSalario(), 0);
     }
+
+    @Test
+    public void testCadastrarSalario3(){
+        irpf.cadastrarSalario(5000f);
+        irpf.cadastrarSalario(6000f);
+        assertEquals(11000f, irpf.getTotalSalario(), 0);
+    }
 }
