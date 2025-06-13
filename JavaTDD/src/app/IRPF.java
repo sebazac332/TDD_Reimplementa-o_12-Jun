6548ec04e18d6d3231ae5ff6e091faf97adf939e
Sebastian Zuzunaga <seb55381@hotmail.com>
2025-06-12 19:20:30 -0500

    segundo teste roda por o principio de falsificacao/duplicacao

--- JavaTDD/src/app/IRPF.java
+++ JavaTDD/src/app/IRPF.java
@@ -39,6 +39,8 @@ public class IRPF {
     public float getDeducaoDependentes(){
         if(dependentes.length == 1){
             return 189.59f;
+        }else if (dependentes.length > 1) {
+            return 379.18f;
         }else{
             return 0f;
         }
